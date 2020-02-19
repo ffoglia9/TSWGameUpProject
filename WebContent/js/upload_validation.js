373c6b3db35c484ac1e88e0b392dff49fe409d89
NobleFM <giuseppericciardi76@gmail.com>
2020-02-19 22:10:26 +0100

    fix regex

--- WebContent/js/upload_validation.js
+++ WebContent/js/upload_validation.js
@@ -1,2 +1,2 @@
-Validations.regs.regex.nomegioco = /^[A-Za-z0-9\-\_/s]{0,60}$/;
-Validations.regs.regex.descrizioneGioco = "/^[A-Za-z0-9\-\_/s]{0,255}$/";
+Validations.regs.regex.nomegioco = /^[A-Za-z0-9\-\_\s]{0,60}$/;
+Validations.regs.regex.descrizioneGioco = "/^[A-Za-z0-9\-\_\s,.]{0,255}$/";
